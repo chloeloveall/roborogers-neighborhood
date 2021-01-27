4278bb65906b682adb4049f8a5f67be01ff81090
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-26 20:20:23 -0800

    Pass TDD test to return an array of numbers from 0 to inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,39 @@
-// Business logic
+// Business Logic
 
 function beepBoop(number) {
-  if (number.includes('3')) {
-    return 'Won\'t you be my neighbor?'
-  } else if (number === '2') {
-    return 'Boop!';
-  } else if (number === '1') {
-    return 'Beep!';
-  } else {
-    return number;
-  }
-};
-console.log(beepBoop('56788876673676879'));
+  let countingArray = [];
 
+  for (let i = 0; i <= number; i++) {
+    countingArray.push(i);
+  }
+  return countingArray;
+};
+
+console.log(beepBoop('23'));
+
+// function beepBoop(number) {
+//   let countingArray = [];
+
+//   for (let i = 0; i <= number; i++) {
+//     if (number.includes('3')) {
+//       countingArray.push('Won\'t you be my neighbor?');
+//     } else if (number.includes('2')) {
+//       countingArray.push('Boop!');
+//     } else if (number.includes('1')) {
+//       countingArray.push('Beep!');
+//     } else {
+//       return number;
+//     }
+//   }
+//   return countingArray;
+// };
+
+// console.log(beepBoop(''));
 
 // User Interface Logic
 
 $(document).ready(function() {
-  $('#form-number').submit(function(event) {
+  $('#form-number').submit(function(event) {P
     event.preventDefault();
 
     const userNumberInput = $('input#userNumber').val();
