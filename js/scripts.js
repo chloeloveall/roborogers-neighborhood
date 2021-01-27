292f4ce12894b6a4807a364e53ff3a2dacf5f204
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-26 22:10:57 -0800

    Pass test to verify input is an integer

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,27 @@
 
 function beepBoop(number) {
   let countingArray = [];
+  
+  if (number % 1 !== 0) {
+    return 'Not a valid entry';
+  }
 
   for (let i = 0; i <= number; i++) {
-    countingArray.push(i);
+    let iString = i.toString();
+    if (iString.includes('3')) {
+      countingArray.push('Won\'t you be my neighbor?');
+    } else if (iString.includes('2')) {
+      countingArray.push('Boop!');
+    } else if (iString.includes('1')) {
+      countingArray.push('Beep!');
+    } else {
+      countingArray.push(i);
+    }
   }
   return countingArray;
 };
 
-console.log(beepBoop('23'));
 
-// function beepBoop(number) {
-//   let countingArray = [];
-
-//   for (let i = 0; i <= number; i++) {
-//     if (number.includes('3')) {
-//       countingArray.push('Won\'t you be my neighbor?');
-//     } else if (number.includes('2')) {
-//       countingArray.push('Boop!');
-//     } else if (number.includes('1')) {
-//       countingArray.push('Beep!');
-//     } else {
-//       return number;
-//     }
-//   }
-//   return countingArray;
-// };
-
-// console.log(beepBoop(''));
 
 // User Interface Logic
 
